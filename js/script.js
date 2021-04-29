ce52ad44feba9fbe4e85d63a2e7bc36c8aecc002
Taka0010 <takayuki.yamato@gmail.com>
2021-04-30 01:27:29 +0900

    init
    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+/*
+// ページのリロードなしのJSを更新する
+if(module.hot){
+  module.hot.accept()
+}
+
+// ES6からできた概念をES5で使えるようにする
+import 'core-js/stable';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
+*/
