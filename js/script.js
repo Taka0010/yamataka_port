67650135cedfec5e01109bb2bde2ce7fd53ae86e
Taka0010 <takayuki.yamato@gmail.com>
2021-05-11 08:30:35 +0900

    ナビ
    ナビつけました。

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-// "use strict";
+'use strict';
 
+//////// HUMBERGER //////////
+const humbIcon = document.querySelector('.header__humb');
+const barUp = document.querySelector('.upper');
+const barDown = document.querySelector('.down');
+const humbMenu = document.querySelector('.header__menu');
+humbIcon.addEventListener('click', function () {
+  console.log('hi');
+  humbMenu.classList.toggle('humbOn');
+});
+
+//////// Swiper.js //////////
 // import Swiper from "swiper/bundle";
 // // import "../node_modules/swiper/swiper-bundle.css";
-const swiper = new Swiper(".workSlider", {
+const swiper = new Swiper('.workSlider', {
   slidesPerView: 1.2,
   spaceBetween: 15,
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: '.swiper-scrollbar',
     hide: true,
   },
 });
+
 /*
 // ページのリロードなしのJSを更新する
 if(module.hot){
