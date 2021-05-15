dcc78db6fc09b1b8e32b97b315776b93e95b911d
Taka0010 <takayuki.yamato@gmail.com>
2021-05-15 10:45:52 +0900

    ナビ＆FVテキスト
    ナビとFVのテキストを調整しました。

--- js/script.js
+++ js/script.js
@@ -10,9 +10,87 @@ humbIcon.addEventListener('click', function () {
   humbMenu.classList.toggle('humbOn');
 });
 
+//////// NAVIGATION //////////
+const header = document.querySelector('.header');
+const headerPc = document.querySelector('.header__menuPc');
+const secSeg = document.querySelector('.secSeg');
+const fstSeg = document.querySelector('.fstSeg');
+const navHeight = header.getBoundingClientRect().height;
+const footer = document.querySelector('.footer');
+
+// メニューフェードアニメーション
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('header__menuPc--list')) {
+    const link = e.target;
+
+    const siblings = link
+      .closest('.header__menuPc--lists')
+      .querySelectorAll('.header__menuPc--list');
+
+    siblings.forEach((el) => {
+      el.style.transition = `opacity .3s ease`;
+      if (el !== link) el.style.opacity = opacity;
+    });
+  }
+};
+header.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.15);
+});
+
+header.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
+
+// テキスト色変え
+const pcNavColoring = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) headerPc.classList.add('coloring');
+  else headerPc.classList.remove('coloring');
+};
+const pcNavObserver = new IntersectionObserver(pcNavColoring, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight / 2}px`,
+});
+pcNavObserver.observe(fstSeg);
+
+// フッターでの消失
+const footerDisappear = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) headerPc.classList.remove('disappear');
+  else headerPc.classList.add('disappear');
+};
+const footerObserver = new IntersectionObserver(footerDisappear, {
+  root: null,
+  threshold: 0,
+});
+footerObserver.observe(footer);
+
+//////// SIDE TEXT //////////
+const sideTxt = document.querySelectorAll('.fstSeg__sideTxt');
+const sideTxtLft = document.querySelector('.left');
+const sideTxtHeight = sideTxtLft.getBoundingClientRect().height;
+
+const sideTxtColorig = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    sideTxt.forEach((el) => {
+      el.classList.add('coloring');
+    });
+  } else {
+    sideTxt.forEach((el) => {
+      el.classList.remove('coloring');
+    });
+  }
+};
+const sideTxtObserver = new IntersectionObserver(sideTxtColorig, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${sideTxtHeight * 2}px`,
+});
+sideTxtObserver.observe(fstSeg);
+
 //////// Swiper.js //////////
-// import Swiper from "swiper/bundle";
-// // import "../node_modules/swiper/swiper-bundle.css";
 const swiper = new Swiper('.workSlider', {
   slidesPerView: 1.2,
   breakpoints: {
